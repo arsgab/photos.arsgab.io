b4dbc90cef4159c92b8a76b113a9059782c2f136
Arseny Gabdullin <a.gabdullin@tinkoff.ru>
2023-04-10 00:44:01 +0200

    Initial commit

---
+++ articles/hello.md
@@ -0,0 +1,4 @@
+Title: Hello
+Date: 2023-04-09 23:00
+
+Sooooo...
---
+++ pelicanconf.py
@@ -0,0 +1,20 @@
+AUTHOR = 'Arseny Gabdullin'
+SITENAME = 'photos.arsgab.io'
+SITEURL = ''
+
+PATH = 'articles'
+THEME = 'assets'
+OUTPUT_PATH = 'dist/'
+DIRECT_TEMPLATES = ['index']
+CATEGORY_SAVE_AS = AUTHOR_SAVE_AS = ''
+
+TIMEZONE = 'Europe/Belgrade'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
