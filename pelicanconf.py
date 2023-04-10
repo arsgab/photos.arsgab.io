3f67d163c30b25af0239cd0b809a2a26dffeea73
Arseny Gabdullin <a.gabdullin@tinkoff.ru>
2023-04-10 12:49:00 +0200

    Configure from env

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,7 +1,11 @@
-AUTHOR = 'arsgab'
-SITENAME = 'photos.arsgab.io'
-TIMEZONE = 'Europe/Belgrade'
+from os import environ as env
 
+
+AUTHOR = env.get('AUTHOR')
+SITENAME = env.get('SITENAME')
+TIMEZONE = env.get('TIMEZONE', 'Europe/Belgrade')
+
+# Disable category/author/feeds pages build
 CATEGORY_SAVE_AS = AUTHOR_SAVE_AS = ''
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
@@ -9,6 +13,7 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
+# Build setup
 DIRECT_TEMPLATES = ['index']
 PATH = 'articles'
 OUTPUT_PATH = 'dist/'
